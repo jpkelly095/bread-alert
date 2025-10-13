027d1027a5ec31d5cca68761d98e34369ecddeaa
James Foley <jpkelly095@gmail.com>
2025-10-13 18:45:01 -0400

    Update Food and add tests

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,9 @@
 class Food < ApplicationRecord
   has_rich_text :notes
 
+  validates :name, :carbs_per_serving, :serving_size, presence: true
+  validates :carbs_per_serving, :serving_size, numericality: { greater_than: 0 }
+
   def carbs_for(amount)
     (amount.fdiv(serving_size) * carbs_per_serving).round
   end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class FoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "carbs for sugar" do
+    sugar = foods(:sugar) # 1 carb per gram
+    res = sugar.carbs_for(50)
+    assert_equal 50, res
+  end
+
+  test "carbs for flour" do
+    flour = foods(:flour) # 23 carbs per 30 grams
+    res = flour.carbs_for(60)
+    assert_equal 46, res
+  end
 end
