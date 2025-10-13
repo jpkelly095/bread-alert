3bdb4ccaa70238b3a2815ac87d2fa5599a6cd6fc
James Foley <jpkelly095@gmail.com>
2025-10-13 19:38:34 -0400

    Add cases for removing Recipe/Meal associations

--- test/models/meal_test.rb
+++ test/models/meal_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class MealTest < ActiveSupport::TestCase
   test "total carbs for breakfast" do
-    assert_equal 60, meals(:breakfast).total_carbs
+    breakfast = meals(:breakfast)
+    assert_equal 60, breakfast.total_carbs
+
+    apple = meal_items(:apple)
+    breakfast.meal_items.delete(apple)
+    assert_equal 46, breakfast.total_carbs # should change when apple is removed
   end
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -2,10 +2,22 @@ require "test_helper"
 
 class RecipeTest < ActiveSupport::TestCase
   test "bagels carbs per serving" do
-    assert_equal 813, recipes(:bagels).carbs_per_serving
+    bagels = recipes(:bagels)
+    assert_equal 813, bagels.carbs_per_serving
+
+    # carbs_per_serving should change when sugar is deleted
+    sugar = ingredients(:bagels_sugar)
+    bagels.ingredients.delete(sugar)
+    assert_equal 767, bagels.carbs_per_serving
   end
 
   test "bagels carbs for" do
-    assert_equal 46, recipes(:bagels).carbs_for(90) # should round to 46
+    bagels = recipes(:bagels)
+    assert_equal 46, bagels.carbs_for(90) # should round to 46
+
+    # carbs_for should change when sugar is deleted
+    sugar = ingredients(:bagels_sugar)
+    bagels.ingredients.delete(sugar)
+    assert_equal 43, bagels.carbs_for(90)
   end
 end
