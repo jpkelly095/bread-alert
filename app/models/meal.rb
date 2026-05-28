9bf3eee862aaba81b24f52347842d7a16920bdfb
James Foley <51831329+jpkelly095@users.noreply.github.com>
2026-05-27 20:02:31 -0400

    Destroy Meal/MealItem scaffolds

--- app/models/meal.rb
+++
@@ -1,7 +0,0 @@
-class Meal < ApplicationRecord
-  has_many :meal_items, dependent: :destroy
-
-  def total_carbs
-    meal_items.sum(&:total_carbs)
-  end
-end
--- db/migrate/20251012213922_create_meals.rb
+++
@@ -1,7 +0,0 @@
-class CreateMeals < ActiveRecord::Migration[8.0]
-  def change
-    create_table :meals do |t|
-      t.timestamps
-    end
-  end
-end
